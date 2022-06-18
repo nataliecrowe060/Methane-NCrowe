b1880c3911ac4aabaf25d61b9855bbdbc59899c2
nataliecrowe060 <106257875+nataliecrowe060@users.noreply.github.com>
2022-06-18 11:38:04 -0700

    hector with rnorm

--- MH_roughDraft.R
+++ MH_roughDraft.R
@@ -10,11 +10,28 @@ France<- gapminder %>%
   summarise(meanlifeExp= mean(lifeExp), 
             sdlifeExp= sd(lifeExp)) 
 
-norm_France<- rnorm(1000, 74.3, 4.30)
-
 data<- data.frame(value= rnorm(1000, 73.3, 4.30))
 
 ggplot(data, aes(x=value))+
   geom_histogram(binwidth = 1)+
   labs( x= "Life Expectancy", y= "count", main= "France Life Expectancy")
 
+#Hector attempt
+library(hector)
+library(ggplot2)
+library(dplyr)
+ini_file <- system.file("input/hector_ssp245.ini", package = "hector")
+core <- newcore(ini_file)
+run(core)
+results<- fetchvars(core, 1745:2100, c(EMISSIONS_CH4()))
+
+emiss<- results %>%
+  select(year, value, units) %>%
+  summarise(meanValue= mean(value), 
+            sdValue= sd(value))
+
+data<- data.frame(value= rnorm(1000, 166.1228, 136.1127))
+
+ggplot(data, aes(x=value))+
+  geom_histogram(binwidth = 1)+
+  labs( x= "Emission Level", y= "count per year", main= "C02 Emissions By Year")
